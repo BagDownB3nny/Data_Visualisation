93c1894a5816abcc4f7f49d0d6a34eeebabf5909
YJ <angyujuan.2001@gmail.com>
2023-11-19 14:22:47 +0000

    remove unneeded files

--- components/time_slider.py
+++
@@ -1,6 +0,0 @@
-from dash import Dash, html, dash_table, dcc
-from datetime import date
-
-def time_slider():
-    slider = dcc.RangeSlider(1, 407, 1, value=[347, 407], id='time-slider', marks=None)
-    return slider
